e97c3ef586a36a23c76c334981a97adc95cf8f2f
EC2 Default User <ec2-user@ip-172-31-16-211.ap-northeast-1.compute.internal>
2023-02-05 23:20:55 +0900

    AddGood&BadBtn(外す挙動の非同期はなし)

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -2,10 +2,10 @@ class GoodsController < ApplicationController
   def create
     @shop = Shop.find(params[:shop_id])
     good = current_user.goods.new(shop_id: @shop.id)
-    good.save
-    if @bad= Bad.find_by(user_id: current_user.id, shop_id: params[:shop_id])
-      @bad.destroy
+    if bad = Bad.find_by(user_id: current_user.id, shop_id: params[:shop_id])
+      bad.destroy
     end
+    good.save
   end
 
   def destroy
